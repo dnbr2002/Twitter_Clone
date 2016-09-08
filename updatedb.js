7107b735aa1d64840fcc5daee99d68c1a2cf9983
dnbr2002 <dnbr2002@yahoo.com>
2016-09-08 15:20:13 -0400

    db create script added

--- updatedb.js
+++ updatedb.js
@@ -2,10 +2,9 @@ var express = require('express');
 var app = express();
 var sqlite3 = require('sqlite3').verbose();
 var db = new sqlite3.Database('TwitterDb.db');
-var name = ['Steve', 'John', 'Kim', 'Sara','Rusty','Scott','Molly','Bryce','Carolyn','Steve'];
+var Author = ['Steve', 'John', 'Kim', 'Sara','Rusty','Scott','Molly','Bryce','Carolyn','Steve'];
 var likestuff = ['cats', 'dogs', 'Kanye', 'cats that like Kanye','Oprah','Horses','Cars','programmming','bikes','skyscrapers'];
 var replies = ['cool', 'not cool', 'me too', 'likewise','always!!','forever','never','ugh','ride or die','yeap!!']
-var randNum = Math.floor((Math.random() * 10) + 1);
 
 
 try {
@@ -18,7 +17,7 @@ try {
         db.serialize(function () {
             var stmt = db.prepare("INSERT INTO Tweet (Date, Text, Author, Replies, Likes) VALUES (?,?,?,?,?)");
             for (var i = 0; i < 10; i++) {
-                stmt.run(current_hour, 'I like ' + likestuff[i], name[i], replies[i], randNum);
+                stmt.run(Math.floor((Math.random() * 10) + 1)+'/'+Math.floor((Math.random() * 10) + 1)+'/2016', 'I like ' + likestuff[i], Author[i], replies[i], Math.floor((Math.random() * 2) + 1));
             }
 
             stmt.finalize();
