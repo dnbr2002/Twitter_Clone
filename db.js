4a891570ce21929f90105229794b169102aa5d57
dnbr2002 <dnbr2002@yahoo.com>
2016-09-08 10:12:50 -0400

    updated db script

--- db.js
+++ db.js
@@ -1,9 +1,9 @@
 var express = require('express');
 var app = express();
 var sqlite3 = require('sqlite3').verbose();
-var db = new sqlite3.Database('test.db');
+var db = new sqlite3.Database('TwitterDb');
 
-app.get('/', function (req, res) {
+/*app.get('/', function (req, res) {
     res.send('Hello World!');
 });
 
@@ -13,22 +13,26 @@ app.get('/foo', function (req, res) {
 
 app.listen(3000, function () {
     console.log('Example app listening on port 3000!');
-});
+});*/
 
+try
+{
+var date = new Date();
+var current_hour = date.getHours();
 
 initDB(db);
 
 function initDB(db) {
     db.serialize(function() {
-        db.run("CREATE TABLE lorem (info TEXT)");
+        db.run("CREATE TABLE Tweet (Date TEXT, Text Text, Author Text, Replies Text, Likes Text)");
 
-        var stmt = db.prepare("INSERT INTO lorem VALUES (?)");
+        var stmt = db.prepare("INSERT INTO TwitterDb VALUES (?)");
         for (var i = 0; i < 10; i++) {
-            stmt.run("Ipsum " + i);
+            stmt.run(current_hour,'blah ' + i, 'Steve the ' + i,'',i);
         }
         stmt.finalize();
 
-        db.each("SELECT rowid AS id, info FROM lorem", function (err, row) {
+        db.each("SELECT rowid AS id, info FROM TwitterDb", function (err, row) {
             if (err) {
                 console.log(err);
             }
@@ -38,3 +42,10 @@ function initDB(db) {
 }
 
 db.close();
+
+}
+
+catch (err)
+{
+    console.log(err);
+}
