ebcd4591f82ece83a2886c337d601375b18c905b
dnbr2002 <dnbr2002@yahoo.com>
2016-09-08 15:53:51 -0400

    update db and db.js file

--- updatedb.js
+++ updatedb.js
@@ -2,27 +2,46 @@ var express = require('express');
 var app = express();
 var sqlite3 = require('sqlite3').verbose();
 var db = new sqlite3.Database('TwitterDb.db');
-var Author = ['Steve', 'John', 'Kim', 'Sara','Rusty','Scott','Molly','Bryce','Carolyn','Steve'];
-var likestuff = ['cats', 'dogs', 'Kanye', 'cats that like Kanye','Oprah','Horses','Cars','programmming','bikes','skyscrapers'];
+var Name = ['Steve', 'John', 'Kim', 'Sara','Rusty','Scott','Molly','Bryce','Carolyn','Steve'];
+var Profile = ['I live in MD', 'I live in DC', 'I love Purple','I live in Florida','I like to surf','I like to play Tennis','','','Must love dogs','']
+var Tweet = ['cats', 'dogs', 'Kanye', 'cats that like Kanye','Oprah','Horses','Cars','programmming','bikes','skyscrapers'];
 var replies = ['cool', 'not cool', 'me too', 'likewise','always!!','forever','never','ugh','ride or die','yeap!!']
 
 
 try {
-    var date = new Date();
-    var current_hour = date.getHours();
+    updateUser(db)
+    updateTweet(db);
 
-    updateDB(db);
-
-    function updateDB(db) {
+    function updateTweet(db) {
         db.serialize(function () {
-            var stmt = db.prepare("INSERT INTO Tweet (Date, Text, Author, Replies, Likes) VALUES (?,?,?,?,?)");
+            var stmt = db.prepare("INSERT INTO Tweet (Date, Text, Author, Reply) VALUES (?,?,?,?)");
             for (var i = 0; i < 10; i++) {
-                stmt.run(Math.floor((Math.random() * 10) + 1)+'/'+Math.floor((Math.random() * 10) + 1)+'/2016', 'I like ' + likestuff[i], Author[i], replies[i], Math.floor((Math.random() * 2) + 1));
+                stmt.run(Math.floor((Math.random() * 10) + 1)+'/'+Math.floor((Math.random() * 10) + 1)+'/2016', 'I like ' + Tweet[i], Name[i], replies[i]);
             }
 
             stmt.finalize();
 
             db.each("SELECT rowid AS id, Text FROM Tweet", function (err, row) {
+                if (err) {
+                    console.log(err);
+                    return;
+                }
+                console.log(row);
+            });
+        });
+
+    }
+
+    function updateUser(db) {
+        db.serialize(function () {
+            var stmt = db.prepare("INSERT INTO User (Name, Profile) VALUES (?,?)");
+            for (var i = 0; i < 10; i++) {
+                stmt.run(Name[i], Profile[i]);
+            }
+
+            stmt.finalize();
+
+            db.each("SELECT rowid AS id, Name FROM User", function (err, row) {
                 if (err) {
                     console.log(err);
                     return;
