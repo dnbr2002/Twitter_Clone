01908c16df86bb0f465a7dc0cbaa5fa852ad8fc6
dnbr2002 <dnbr2002@yahoo.com>
2016-09-08 14:11:09 -0400

    updated insert for Tweet

--- updatedb.js
+++ updatedb.js
@@ -1,7 +1,12 @@
 var express = require('express');
 var app = express();
 var sqlite3 = require('sqlite3').verbose();
-var db = new sqlite3.Database('TwitterDb');
+var db = new sqlite3.Database('TwitterDb.db');
+var name = ['Steve', 'John', 'Kim', 'Sara','Rusty','Scott','Molly','Bryce','Carolyn','Steve'];
+var likestuff = ['cats', 'dogs', 'Kanye', 'cats that like Kanye','Oprah','Horses','Cars','programmming','bikes','skyscrapers'];
+var replies = ['cool', 'not cool', 'me too', 'likewise','always!!','forever','never','ugh','ride or die','yeap!!']
+var randNum = Math.floor((Math.random() * 10) + 1);
+
 
 try {
     var date = new Date();
@@ -13,7 +18,7 @@ try {
         db.serialize(function () {
             var stmt = db.prepare("INSERT INTO Tweet (Date, Text, Author, Replies, Likes) VALUES (?,?,?,?,?)");
             for (var i = 0; i < 10; i++) {
-                stmt.run(current_hour, 'blah ' + i, 'Steve the ' + i, '', 'y');
+                stmt.run(current_hour, 'I like ' + likestuff[i], name[i], replies[i], randNum);
             }
 
             stmt.finalize();
