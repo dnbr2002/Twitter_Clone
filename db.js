ddcbe402a12165c7bb8195ec06351e8b248687a2
dnbr2002 <dnbr2002@yahoo.com>
2016-09-13 16:44:20 -0400

    added

--- db.js
+++ db.js
@@ -1,51 +1,10 @@
+"use strict";
 var express = require('express');
 var app = express(); //may not need this here
 var sqlite3 = require('sqlite3').verbose();
 var db = new sqlite3.Database('TwitterDb.db');
-var TweetTable = 'Tweet'
-var UserTable = 'User'
 
-
-//This is not correct!!!
-
-CreateTweetTable(db);
-function CreateTweetTable(db) {
-    try {
-        db.serialize(function () {
-
-            db.run("CREATE TABLE IF NOT EXISTS " + TweetTable + " (PK_Tweet Integer NOT NULL PRIMARY KEY,Date TEXT, Text Text, Author Text, Replies Text, Likes Text)");
-                
-                console.log("Tweet table created");
-    });
-
-
-    }
-
-    catch (err) {
-        console.log(err);
-    }
-   CreateUserTable(db);
-    function CreateUserTable(db) {
-    try {
-        db.serialize(function () {
-
-            db.run("CREATE TABLE IF NOT EXISTS " + UserTable + " (Pk_User Integer NOT NULL PRIMARY KEY, Profile Text, Name Text, History Text, Followers Text, Following Text)");
-
-                
-                console.log("User table created");
-    });
-
-        db.close();
-    }
-    
-    catch (err) {
-        console.log(err);
-    }
-    }
-}
-
-
-function allFollowingTweets(Username){
+function GetUserId(Username){
     return new Promise(function (resolve,reject) {
         db.each("SELECT PK_User User where name="+Username+"", function (err, row) {
             if (err) {
@@ -58,3 +17,49 @@ function allFollowingTweets(Username){
         });
     });
 }
+
+FollowUser(db,FollowName,FollowingName);
+function FollowUser(db,FollowName,FollowingName){
+    return new Promise(function (resolve, reject) {
+        db.run("INSERT INTO Following (Follower, Followee) VALUES ('"+FollowName+"','"+FollowingName+"')", function (err) {
+            if (err) {
+                console.log(err);
+                reject(err);
+            }else {
+                console.log("Successfully Following "+FollowName+" Now");
+                resolve();
+            }
+        });
+    });
+}
+function UnFollowUser(db, FollowName, FollowingName) {
+    return new Promise(function (resolve, reject) {
+        db.run("DELETE FROM Following WHERE Follower='" + FollowName + "' AND Followee='" + FollowingName + "'", function (err) {
+            if (err) {
+                console.log(err);
+                reject(err);
+            } else {
+                console.log("Successfully UnFollowing " + FollowingName + " Now");
+                resolve();
+            }
+        });
+    });
+}
+
+SelectUserProfile(db, 'Rusty');
+function SelectUserProfile(db,SelectedUser){
+    return new Promise(function (resolve,reject) {
+        db.each("SELECT name, profile, T.Text from User U join Tweet T where U.Name=T.Author and U.Name='"+SelectedUser+"'", function (err, row) {
+            if (err) {
+                reject(err);
+                console.log(err);
+
+            }else {
+                console.log(row);
+                resolve();
+            }
+        });
+    });
+}
+
+
--- tweet.js
+++ tweet.js
@@ -8,51 +8,6 @@ var FollowName = 'Kim';
 var FollowingName = 'Rusty';
 
 
-FollowUser(db,FollowName,FollowingName);
-function FollowUser(db,FollowName,FollowingName){
-    return new Promise(function (resolve, reject) {
-        db.run("INSERT INTO Following (Follower, Followee) VALUES ('"+FollowName+"','"+FollowingName+"')", function (err) {
-            if (err) {
-                console.log(err);
-                reject(err);
-            }else {
-                console.log("Successfully Following "+FollowName+" Now");
-                resolve();
-            }
-    });
-    });
-}
-function UnFollowUser(db, FollowName, FollowingName) {
-    return new Promise(function (resolve, reject) {
-        db.run("DELETE FROM Following WHERE Follower='" + FollowName + "' AND Followee='" + FollowingName + "'", function (err) {
-            if (err) {
-                console.log(err);
-                reject(err);
-            } else {
-                console.log("Successfully UnFollowing " + FollowingName + " Now");
-                resolve();
-            }
-        });
-    });
-}
-
-SelectUserProfile(db, 'Rusty');
-function SelectUserProfile(db,SelectedUser){
-    return new Promise(function (resolve,reject) {
-        db.each("SELECT name, profile, T.Text from User U join Tweet T where U.Name=T.Author and U.Name='"+SelectedUser+"'", function (err, row) {
-            if (err) {
-                reject(err);
-                console.log(err);
-
-            }else {
-                console.log(row);
-                resolve();
-            }
-        });
-    });
-}
-
-
 
 
 
